dcd1f5cf3ffd462be2bb46f59a97850cee8ca885
phcarneiro9 <ph.carneiro9@gmail.com>
2026-06-12 11:47:12 -0300

    Aula2_Exercício_2

---
+++ Aula_02_operadores/src/Aula_02_operadores/exercício2.java
@@ -0,0 +1,39 @@
+package Aula_02_operadores;
+
+import java.util.Scanner;
+
+public class exercício2 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float nota1;
+		float nota2;
+		float nota3;
+		float nota4;
+		float Media;
+	
+		System.out.println("Digite a primeira nota:");
+		nota1 = leia.nextFloat();
+		
+		System.out.println("DDigite a segunda nota:");
+		nota2 = leia.nextFloat();
+		
+		System.out.println("Digite a terceira nota:");
+		nota3 = leia.nextFloat();
+
+		System.out.println("DDigite a quarta nota:");
+		nota4 = leia.nextFloat();
+		
+		
+		Media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("Média final: " + Media);
+		
+		leia.close();
+
+		
+	}
+
+}
