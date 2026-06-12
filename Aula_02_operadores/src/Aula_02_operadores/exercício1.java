64b6cf8b17910a93551e8f9607307ecc80fcc3ee
phcarneiro9 <ph.carneiro9@gmail.com>
2026-06-12 11:27:14 -0300

    Aula_02_Exercício_1

---
+++ Aula_02_operadores/src/Aula_02_operadores/exercício1.java
@@ -0,0 +1,32 @@
+package Aula_02_operadores;
+
+import java.util.Scanner;
+
+public class exercício1 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float Salário;
+		float Abono;
+		float novoSalário;
+		
+		System.out.println("Digite o salário:");
+		Salário = leia.nextFloat();
+		
+		System.out.println("Digite o abono:");
+		Abono = leia.nextFloat();
+		
+		novoSalário = Salário + Abono;
+
+        System.out.println("Novo salário: " + novoSalário);
+
+		
+
+		
+		
+
+	}
+
+}
